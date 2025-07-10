d25a7278ea73a382ed0080988093db16716603e8
Masaru Gotou <masaru.gotou@gmail.com>
2025-07-10 08:22:27 +0000

    HasGameReferenceを使用する（コードレビュー指摘対応） Fixes #2

--- life_game_app/lib/game.dart
+++ life_game_app/lib/game.dart
@@ -6,37 +6,37 @@ class LifeGame extends FlameGame with HasCollisionDetection {
   @override
   Future<void> onLoad() async {
     await super.onLoad();
-    
+
     add(MovingSquare());
   }
 }
 
-class MovingSquare extends RectangleComponent {
+class MovingSquare extends RectangleComponent with HasGameReference<LifeGame> {
   static const double speed = 100;
   late Vector2 velocity;
-  
+
   @override
   Future<void> onLoad() async {
     super.onLoad();
-    
+
     size = Vector2(50, 50);
     position = Vector2(100, 100);
     paint.color = Colors.blue;
-    
+
     velocity = Vector2(speed, speed);
   }
-  
+
   @override
   void update(double dt) {
     super.update(dt);
-    
+
     position += velocity * dt;
-    
-    if (position.x <= 0 || position.x >= (parent as LifeGame).size.x - size.x) {
+
+    if (position.x <= 0 || position.x >= game.size.x - size.x) {
       velocity.x *= -1;
     }
-    
-    if (position.y <= 0 || position.y >= (parent as LifeGame).size.y - size.y) {
+
+    if (position.y <= 0 || position.y >= game.size.y - size.y) {
       velocity.y *= -1;
     }
   }
