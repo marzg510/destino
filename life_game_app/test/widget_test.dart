38bb0e39a5327bfe6d41f9003ec25a4d94db6a95
Masaru Gotou <masaru.gotou@gmail.com>
2025-09-25 08:12:54 +0000

    [Test] フェーズ1の統合テストとポリッシュ Fixes #14 テストコードを追加

--- life_game_app/test/widget_test.dart
+++ life_game_app/test/widget_test.dart
@@ -1,15 +1,21 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:life_game_app/main.dart';
+import 'package:flame/game.dart';
+import 'package:life_game_app/game/my_game.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {});
+  testWidgets('Game widget renders without crashing', (WidgetTester tester) async {
+    final game = MyGame();
+    await tester.pumpWidget(
+      MaterialApp(
+        home: GameWidget<MyGame>.controlled(gameFactory: () => game),
+      ),
+    );
+
+    // 初期レンダリングを実行
+    await tester.pump(const Duration(milliseconds: 100));
+
+    // MaterialAppが存在することを確認
+    expect(find.byType(MaterialApp), findsOneWidget);
+  });
 }
